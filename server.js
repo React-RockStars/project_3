014f46c683333ab3cbc1ed23bec913b377d6c965
laurenmenendez <menendez98@gmail.com>
2021-05-10 14:39:50 -0400

    Replace "example" with post and comment routes

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
+const postRoutes = require('./app/routes/post_routes')
+const commentRoutes = require('./app/routes/comment_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -56,7 +57,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
+app.use(postRoutes)
+app.use(commentRoutes)
 app.use(userRoutes)
 
 // register error handling middleware
